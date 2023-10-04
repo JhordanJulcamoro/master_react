073680d60e8119496a0f1372cebcf0aca733d8b4
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-04 12:23:14 -0500

    imprime data de variables - json

--- primeros_pasos/src/MiComponente.js
+++ primeros_pasos/src/MiComponente.js
@@ -4,9 +4,14 @@ import React, { Fragment } from "react";
 // funcion del componente
 const MiComponente = () => {
 
-    let nombre = "Jhordan Julcamoro";
+    let nombre = "Jhordan";
     let web = "http://julhua.com";
 
+    let usuario = {
+        nombre: nombre,
+        apellidos: "Julcamoro",
+        web: web
+    }
     return (
         <div className="mi-componente">
             <hr />
@@ -15,6 +20,12 @@ const MiComponente = () => {
             <ul>
                 <li>Nombre: <strong> {nombre}</strong></li>
                 <li>Web:  <strong> {web}</strong></li>
+            </ul>
+            <h3>Datos del usuario - JSON:</h3>
+            <ul>
+                <li>Nombre: <strong> {usuario.nombre}</strong></li>
+                <li>Apellidos: <strong> {usuario.apellidos}</strong></li>
+                <li>Web:  <strong> {usuario.web}</strong></li>
             </ul>
             <p> este es mi primer componente</p>
             <ul>
