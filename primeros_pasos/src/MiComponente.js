46deb3af8dd148f5e45ded6a5549ce944e23dd5b
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-04 12:07:31 -0500

    Creando primer componente

---
+++ primeros_pasos/src/MiComponente.js
@@ -0,0 +1,10 @@
+// Importar modulos de react / dependencias
+import React from "react";
+
+// funcion del componente
+const MiComponente = () => { 
+    return <p> este es mi primer componente</p>
+}
+
+// exportar componente
+export default MiComponente;
