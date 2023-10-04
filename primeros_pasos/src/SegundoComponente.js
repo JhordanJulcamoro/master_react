7eccb07201f0b0b168827fb9109576bfe1ca0e43
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-04 12:37:07 -0500

    valida si hay algo dentro del array libros

--- primeros_pasos/src/SegundoComponente.js
+++ primeros_pasos/src/SegundoComponente.js
@@ -2,20 +2,29 @@ import React from 'react'
 
 export const SegundoComponente = () => {
 
-    const libros = [
-        "Harry Potter",
-        "Juego de tronos",
-        "Clean code"
-    ]
+    // const libros = [
+    //     "Harry Potter",
+    //     "Juego de tronos",
+    //     "Clean code"
+    // ]
 
-  return (
-    <div className='segundo-componente'>
-        <h1> Listado de libros</h1>
-       <ul>
-        <li>{libros[0]}</li>
-        <li>{libros[1]}</li>
-        <li>{libros[2]}</li>
-        </ul>
-    </div>
-  )
+    const libros = []
+
+    return (
+        <div className='segundo-componente'>
+            <h1> Listado de libros</h1>
+
+            {libros.length >= 1 ?
+                <ul>
+                    {
+                        libros.map((libro, indice) => {
+                            return <li key={indice}>{libro}</li>
+                        })
+                    }
+                </ul>
+                : 
+                <p>No hay libros disponibles</p>
+            }
+        </div>
+    )
 }
