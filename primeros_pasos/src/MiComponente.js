f722b2f92d696a0e9bb8352c436bff4fb3b2e353
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-04 12:13:05 -0500

    componente dentro de div

--- primeros_pasos/src/MiComponente.js
+++ primeros_pasos/src/MiComponente.js
@@ -2,8 +2,25 @@
 import React from "react";
 
 // funcion del componente
-const MiComponente = () => { 
-    return <p> este es mi primer componente</p>
+const MiComponente = () => {
+    return (
+        <div>
+            <hr />
+            <h2> Creando componente</h2>
+            <p> este es mi primer componente</p>
+            <ul>
+                <li>
+                    React
+                </li>
+                <li>
+                    Angular
+                </li>
+                <li>
+                    Vue
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 // exportar componente
