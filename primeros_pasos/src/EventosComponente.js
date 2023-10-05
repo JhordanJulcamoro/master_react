d8502ee65adc8e89f1cb3e9489d147667ae2abe8
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-05 12:11:36 -0500

    onclick utilizando funciones

--- primeros_pasos/src/EventosComponente.js
+++ primeros_pasos/src/EventosComponente.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 export const EventosComponente = () => {
+
+  const hasDadoClick = () => {
+    alert('Has Dado Click');
+  }
+
   return (
     <div>
         <h1>Eventos en React</h1>
 
         {/* Evento click */}
         <h3>Evento onclick:: </h3>
-        <button onClick={(e) => {
-            console.log(e);
-            console.log("Hola soy un evento click")
-        }}>Dame Click</button>
+        <button onClick={hasDadoClick}>Dame Click</button>
     </div>
   )
 }
