6e0f791dbcd7caad8480a58f9d8e8ae01ef3e71c
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-07 11:26:15 -0500

    useEffect - se ejecuta solo al cargar el componente (1 sola vez)

--- use-effect/src/components/PruebasComponet.js
+++ use-effect/src/components/PruebasComponet.js
@@ -2,6 +2,7 @@ import React, {useState,useEffect} from 'react'
 
 export const PruebasComponet = () => {
   const [usuario,setUsuario] = useState('Jhordan Julcamoro');
+  const [fecha, setFecha] = useState('01-01-1997');
 
   const modUsuario = e => {
     setUsuario(e.target.value);
@@ -9,19 +10,25 @@ export const PruebasComponet = () => {
   }
 
   useEffect(() => {
-    console.log('Has cargado o modificado el componente');
-  })
+    console.log('Has cargado el componente');
+  }, [])
 
-    
+  const cambiarFecha = e => {
+    setFecha(Date.now())
+  }
+
   return (
     <div>
       <h1>El efecto - hook useEffect</h1>
 
       <strong>{usuario}</strong>
+      
+      <strong> {fecha}</strong>
       <p>
         <input type='text'
           onChange={modUsuario}
           placeholder='Cambia el nombre'/>
+        <button onClick={cambiarFecha}>Cambiar fecha</button>
       </p>
     </div>
   )
