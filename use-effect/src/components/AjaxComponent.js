58348c15643cdf60c0f0dd5d03badf25051120ab
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-08 15:29:37 -0500

    AJAX :: useEffect muestra datos estaticos en la primera vez que se carga el componente o es decir, al cargar el componente

---
+++ use-effect/src/components/AjaxComponent.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+
+
+export const AjaxComponent = () => {
+    const [usuarios, setUsuarios] = useState([]);
+
+    const getUsuariosEstaticos = () => {
+        setUsuarios([
+            {
+                "id": 1,
+                "email": "jhordan@jhordan.com",
+                "first_name": "Jhordan",
+                "last_name": "Julcamoro"
+            },
+            {
+                "id": 2,
+                "email": "jhordan2@jhordan.com",
+                "first_name": "Jhordan2",
+                "last_name": "Julcamoro2"
+            },
+            {
+                "id": 3,
+                "email": "jhordan3@jhordan.com",
+                "first_name": "Jhordan3",
+                "last_name": "Julcamoro3"
+            },
+            {
+                "id": 4,
+                "email": "jhordan4@jhordan.com",
+                "first_name": "Jhordan4",
+                "last_name": "Julcamoro4"
+            }
+        ])
+    }
+
+    useEffect(()=>{
+        //cada vez que se carga el componente, se llena el array
+        getUsuariosEstaticos();
+    },[]);
+
+    //mediante una promesa   
+    return (
+        <div>
+            <h1>Listado de usuarios via AJAX</h1>
+            <ol className='usuarios'>
+                {
+                    usuarios.map(usuarios => {
+                        return <li key={usuarios.id}>{usuarios.first_name} {usuarios.last_name}</li>
+                    })
+                }
+            </ol>
+        </div>
+    )
+}
