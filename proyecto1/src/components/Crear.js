7c8f27775b7f859eaf9cb87efc1cdd04a411f741
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-09 23:19:18 -0500

    componente crear: obtiene informacion del formulario

--- proyecto1/src/components/Crear.js
+++ proyecto1/src/components/Crear.js
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Crear = () => {
+
+    let tituloComponente = "Añadir pelicula";
+    const [peliculaState, setPeliculaState] = useState({
+        id:'',
+        titulo:'',
+        descripcion:''
+    });
+
+    //desestructurar una variable
+    const {titulo, descripcion} = peliculaState;
+
+    const conseguirDatosForm = e => {
+        e.preventDefault();
+        
+        let target = e.target;
+        let titulo =target.titulo.value;
+        let descripcion = target.descripcion.value;
+
+        let pelicula = {
+            id: new Date().getTime(),
+            titulo,
+            descripcion
+        }
+
+        setPeliculaState(pelicula);
+
+        console.log(peliculaState)
+    }
+
     return (
         <div className="add">
-            <h3 className="title">Añadir pelicula</h3>
-            <form>
-                <input type="text" id="title" placeholder="Titulo" />
-                <textarea id="description" placeholder="Descripción"></textarea>
-                <input type="submit" id="save" value="Guardar" />
+            <h3 className="title">{tituloComponente}</h3>
+           {(titulo && descripcion) && "Haz creado: " + titulo}
+            <form onSubmit={conseguirDatosForm}>
+                <input
+                    type="text"
+                    id="titulo"
+                    placeholder="Titulo"
+                    name='titulo' />
+                <textarea
+                    id="descripcion"
+                    placeholder="Descripción"
+                    name='descripcion'>
+                </textarea>
+                <input
+                    type="submit"
+                    id="save"
+                    value="Guardar"
+                    name='save' />
             </form>
         </div>
     )
