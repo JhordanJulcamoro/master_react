9d22618a8e3762adf0011113f211fd2ca37dce1f
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-09 22:27:11 -0500

    FORM :: Actualiza datos dinamicamente, en caso hayan cambios, los actuaiza automaticamente

--- formularios/src/components/FormularioComponent.js
+++ formularios/src/components/FormularioComponent.js
@@ -3,7 +3,7 @@ import { UsuarioComponent } from './UsuarioComponent';
 
 export const FormularioComponent = () => {
 
-    const [usuario, setUsuario] = useState([]);
+    const [usuario, setUsuario] = useState({});
 
     const sendData = (e) => {
         e.preventDefault();
@@ -12,30 +12,50 @@ export const FormularioComponent = () => {
             nombre: data.nombre.value,
             apellido: data.apellido.value,
             genero: data.genero.value,
-            biografia: data.bio.value,
+            biografia: data.biografia.value,
+            enviar: data.enviar.value,
         }
 
         console.log(usuario)
         setUsuario(usuario);
     }
+
+    const cambiarDatos = e => {
+        let name_input = e.target.name;
+
+        // Primera forma de return
+        // setUsuario(estado_previo => {
+        //     return {
+        //         ...estado_previo,
+        //         [name_input]: e.target.value
+        //     }
+        // })
+
+        // Segunda forma de return
+        setUsuario(estado_previo => ({
+            ...estado_previo,
+            [name_input]: e.target.value
+        }))
+
+        console.log(usuario);
+    }
     return (
         <div>
             <h1>Formularios con REACT</h1>
-            {usuario.biografia &&
-                usuario.biografia.length >= 1 &&
+            {usuario.enviar &&
                 (
                     <UsuarioComponent usuario={usuario}></UsuarioComponent>
                 )}
 
             <form onSubmit={sendData}>
-                <input type="text" placeholder='Nombre: ' name='nombre' />
-                <input type="text" placeholder="Apellido: " name='apellido' />
-                <select name='genero'>
+                <input type="text" placeholder='Nombre: ' name='nombre' onChange={cambiarDatos} />
+                <input type="text" placeholder="Apellido: " name='apellido' onChange={cambiarDatos} />
+                <select name='genero' onChange={cambiarDatos}>
                     <option value="Hombre"> Hombre</option>
                     <option value="Mujer">Mujer</option>
                 </select>
-                <textarea placeholder='Biografia' name='bio'></textarea>
-                <input type='submit' value="Enviar" />
+                <textarea placeholder='Biografia' name='biografia' onChange={cambiarDatos}></textarea>
+                <input type='submit' value="Enviar" name='enviar' />
             </form>
         </div>
     )
